cb6477ab70cf7888fab1071c22991d9209135f28
Hemant <hemant121222@gmail.com>
2021-03-18 01:11:54 +0530

    Compleate teams model setup

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'CarZone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CarZone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Monika@222',
+        'HOST': 'localhost',
+
     }
 }
 
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width"40px" height= "40px" style="border-radius:50px" alt=""/>'.format(object.photo.url))
+
+    thumbnail.short_description = "Photo"
+
+    list_display = ('id', 'thumbnail', 'first_name', 'last_name', 'designation', 'created_date')
+    list_display_links = ('id','thumbnail', 'first_name')
+
+    # For Search bar
+    search_fields = ('id', 'designation', 'first_name', 'last_name')
+    list_filter = ('designation',)
+
+
+
+admin.site.register(Team, TeamAdmin)
+
