ebda1e9beb8b9eb35f3bbf6bb2d6e9ec416995d7
Hemant <hemant121222@gmail.com>
2021-03-19 02:09:37 +0530

    Add car apps data and fetch the data into the homepage of the CarZone

--- CarZone/settings.py
+++ CarZone/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Added Manually 
     'ckeditor',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Car
+from django.utils.html import format_html
+
 # Register your models here.
 
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width"40px" height= "40px" style="border-radius:50px" alt=""/>'.format(object.car_photo.url))
+
+    thumbnail.short_description = "Car Image"
+
+    list_display = ('id', 'thumbnail', 'car_title', 'city', 'color', 'model', 'year', 'body_style','fuel_type','is_featured')
+    list_display_links = ('id','thumbnail', 'car_title')
+
+    # For Search bar
+    search_fields = ('id', 'car_title', 'model', 'year')
+    list_filter = ('car_title',)
+
+admin.site.register(Car, CarAdmin)
